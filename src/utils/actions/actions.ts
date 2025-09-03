b8dd4d64a8c52a86d5ffe4ad46a769bfed3cb1f6
Anchal Mishra <mishra890anchal@gmail.com>
2025-09-03 15:03:27 +0530

    add loginAction

--- src/utils/actions/actions.ts
+++ src/utils/actions/actions.ts
@@ -1,10 +1,9 @@
 "use server";
-
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-
 export async function Loginaction(formData: FormData) {
   try {
+    const cookieStore = await cookies();
     const res = await fetch(`${process.env.BACKEND_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -13,34 +12,33 @@ export async function Loginaction(formData: FormData) {
         password: formData.get("password"),
       }),
     });
-
     const data = await res.json();
     if (!res.ok) {
       throw new Error(data.message || "Login failed");
     }
-
-    // Store token in a cookie or session here if needed
-    // e.g. setCookie("orgToken", data.token, { httpOnly: true });
-
-    // Redirect from server after login success
+    console.log("data is", data);
+    cookieStore.set("token", data.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+    });
     redirect("/");
   } catch (error) {
     console.error("Got error while signing in", error);
     throw error;
   }
 }
-
 export async function registerAction(formData: FormData, code?: string) {
   try {
+    const cookieStore = await cookies();
     const payload = {
       firstName: formData.get("firstName"),
       lastName: formData.get("lastName"),
       email: formData.get("email"),
       password: formData.get("password"),
     } as Record<string, string>;
-
     if (code) payload.code = code;
-
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register`,
       {
@@ -50,21 +48,16 @@ export async function registerAction(formData: FormData, code?: string) {
         cache: "no-store",
       }
     );
-
     const data = await response.json();
-
     if (!response.ok) {
       throw new Error(data.message || "Signup failed");
     }
-
-    // Store token in HttpOnly cookie
-    cookies().set("token", data.token, {
+    cookieStore.set("token", data.token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       path: "/",
     });
-
     // Redirect user straight to dashboard (already logged in)
     redirect("/");
   } catch (error: any) {
