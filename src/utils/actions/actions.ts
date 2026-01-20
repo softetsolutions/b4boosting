6da46339b3fddf33630b446b51b2cdadcfc1bc10
ruchi-intileo <ruchi.kumari63@gmail.com>
2026-01-20 05:34:51 +0530

    cookie issue fixes

--- src/utils/actions/actions.ts
+++ src/utils/actions/actions.ts
@@ -2,11 +2,10 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
-
 export async function Loginaction(formData: FormData) {
   try {
     const cookieStore = await cookies();
-    
+
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
       {
@@ -16,7 +15,7 @@ export async function Loginaction(formData: FormData) {
           email: formData.get("email"),
           password: formData.get("password"),
         }),
-      }
+      },
     );
     const data = await res.json();
     if (!res.ok) {
@@ -24,25 +23,30 @@ export async function Loginaction(formData: FormData) {
     }
     console.log("data is", data);
     cookieStore.set("token", data.token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
+      domain: ".b4boosting.com", // ⭐ MOST IMPORTANT
       path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-     cookieStore.set("userId", data.user.id, {
-      httpOnly: false,
+    cookieStore.set("userId", data.user.id, {
+      httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
+      domain: ".b4boosting.com", // ⭐ MOST IMPORTANT
       path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     cookieStore.set("affiliateId", data.user.affiliateId, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
+      domain: ".b4boosting.com", // ⭐ MOST IMPORTANT
       path: "/",
-    })
-   redirect(data.user.role === "admin" ? "/admin" : `/`);
-
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    redirect(data.user.role === "admin" ? "/admin" : `/`);
   } catch (error) {
     console.error("Got error while signing in", error);
     throw error;
@@ -53,16 +57,21 @@ export async function registerAction(formData: FormData) {
 
   try {
     const cookieStore = await cookies();
-    
-    console.log(cookieStore,"cookieStore", process.env.NEXT_PUBLIC_BACKEND_URL, "process.env.NEXT_PUBLIC_BACKEND_URL");
+
+    console.log(
+      cookieStore,
+      "cookieStore",
+      process.env.NEXT_PUBLIC_BACKEND_URL,
+      "process.env.NEXT_PUBLIC_BACKEND_URL",
+    );
     const payload = {
       firstName: formData.get("firstName"),
       lastName: formData.get("lastName"),
       email: formData.get("email"),
       password: formData.get("password"),
-      affiliateRef: cookieStore.get("affiliateRef")?.value
+      affiliateRef: cookieStore.get("affiliateRef")?.value,
     } as Record<string, string>;
-   
+
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register`,
       {
@@ -71,7 +80,7 @@ export async function registerAction(formData: FormData) {
         body: JSON.stringify(payload),
         cache: "no-store",
         credentials: "include",
-      }
+      },
     );
     console.log("response of signup is", response);
     const data = await response.json();
@@ -81,14 +90,16 @@ export async function registerAction(formData: FormData) {
     cookieStore.set("token", data.token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
+      domain: ".b4boosting.com", // ⭐ MOST IMPORTANT
       path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-   
+
     redirect("/login");
   } catch (error) {
     console.error("Error in registerAction:", error);
-   throw error;
+    throw error;
   }
 }
 
